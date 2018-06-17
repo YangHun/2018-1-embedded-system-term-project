af94b8b9be5adeac28c866721c6485e8cd29d562
Noverish Harold <embrapers263@gmail.com>
2018-06-17 23:00:52 +0900

    var to const in app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -44,7 +44,7 @@ app.use(function(err, req, res, next) {
 // ------- creates Server -------
 module.exports = app;
 
-var server = app.listen(app.get('port'), function() {
+const server = app.listen(app.get('port'), function() {
 console.log('Express server listening on port ' + server.address().port);
 });
 
