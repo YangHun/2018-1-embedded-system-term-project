dfd3cad1c41f2da2d4b4b5894a82c43a2f6351fc
Noverish Harold <embrapers263@gmail.com>
2018-06-17 22:59:07 +0900

    Removed comments

--- routes/index.js
+++ routes/index.js
@@ -6,24 +6,6 @@ const web3 = new Web3(new Web3.providers.HttpProvider('http://52.78.219.223:8123
 
 const util = require('util');
 
-function getFirstAccount() {
-    return new Promise(function(resolve, reject) {
-        web3.eth.getAccounts(function (err, accounts) {
-            resolve(accounts[0]);
-        })
-    })
-}
-/*
-AddLog(eth.accounts[1], "asdf", "1234", {from: eth.coinbase}) return
-*/
-function getContract(account) {
-    return new Promise(function(resolve, reject) {
-
-
-        resolve(account, contract);
-    })
-}
-
 /* GET home page. */
 router.get('/', function (req, res, next) {
 
@@ -42,90 +24,25 @@ router.get('/', function (req, res, next) {
             for (i = 0; i < logNum; i++) {
                 contract.methods.getLog(account, i).call().then(function (result) {
                     results.push(result);
-                    if(results.length == logNum) {
+                    if (results.length == logNum) {
                         res.json(results);
                     }
                 });
             }
 
-            if(logNum == 0){
+            if (logNum == 0) {
                 res.json([]);
             }
         });
-
     });
-
-    // let arr = Array();
-    // arr.push("asdf1");
-    // arr.push("asdf2");
-    // arr.push("asdf3");
-    // arr.push("asdf4");
-    // res.json(arr);
-
-    /*
-    function getLogNum(account, contract) {
-        return new Promise(function(resolve, reject) {
-
-            const tmp = util.inspect(contract);
-            console.log(tmp);
-
-            contract.methods.numLogs(account).call().then(function (count) {
-
-                console.log("count " + count);
-
-                resolve(count, account, contract);
-            })
-        })
-    }
-
-    function getLogAt(index, account, contract) {
-        return new Promise(function(resolve, reject) {
-            contract.methods.getLog(account, index).call().then(function (result) {
-
-
-                resolve(result)
-            });
-        })
-    }
-
-    function getLogList(logNum, account, contract) {
-        return new Promise(function(resolve, reject) {
-            let results = Array();
-
-            let i;
-            for (i = 0; i < logNum; i++) {
-                contract.methods.getLog(account, i).call().then(function (result) {
-                    console.log(i + " " + result);
-
-                    results.push(result);
-                });
-
-                if (i === account - 1) {
-                    resolve(results);
-                }
-            }
-        })
-    }
-
-    getFirstAccount()
-        .then(getContract)
-        .then(getLogNum)
-        .then(getLogList)
-        .then(results => {
-          res.json(results)
-        })
-        */
-
-    // var body = util.inspect(web3);
-    // res.render('index', { title: 'BlockChain of Noverish!!!', body: body });
 });
 
 router.post('/', function (req, res, next) {
     const content = req.body.content;
     const time = req.body.time;
 
-    console.log("content : " + content);
-    console.log("time : " + time);
+    console.log('content : ' + content);
+    console.log('time : ' + time);
 
     web3.eth.getAccounts(function (err, accounts) {
         const account0 = accounts[0];
@@ -136,60 +53,23 @@ router.post('/', function (req, res, next) {
         const contract_address = '0x4701dB4Ca28F37560192D0Ea043091bfc0C2D8C2';
         const contract = new web3.eth.Contract(contract_object, contract_address);
 
-        // console.log(account0);
-
-        web3.eth.personal.unlockAccount(account0, "1234")
+        web3.eth.personal.unlockAccount(account0, '1234')
             .then((response) => {
 
-                var tmp = {from: account0, gasPrice: "72000000000", gas: "180000"};
+                const options = { from: account0, gasPrice: '72000000000', gas: '180000' };
 
-                contract.methods.AddLog(account1, content, time).send(tmp, function(error, result) {
-                    if(error) {
-                        res.json("error2 : " + error);
+                contract.methods.AddLog(account1, content, time).send(options, function (error, result) {
+                    if (error) {
+                        res.json('error2 : ' + error);
                     } else {
-                        console.log("result : " + result);
+                        console.log('result : ' + result);
                         res.json(result);
                     }
-                })
-                    // .then(function(result) {
-                    //     res.json(result);
-                    // }).catch((error) => {
-                    //     res.json("error2 : " + error);
-                    // });
-
-                // res.json("response : " + response);
+                });
             }).catch((error) => {
-                res.json("error1 : " + error);
-            });
-
-
-
-        // const tmp = util.inspect(web3.eth.personal);
-
-
+            res.json('error1 : ' + error);
+        });
     });
-
-    /*
-
-    function addLog(account, contract) {
-        return new Promise(function (resolve, reject) {
-            contract.methods.addLogs(account, content, "1234").call().then(function (result) {
-                resolve(result);
-            });
-        })
-    }
-
-    getFirstAccount()
-        .then(getContract)
-        .then(addLog)
-        .then(result => {
-            res.json(result)
-        })
-        */
-
-
-    // var body = util.inspect(web3);
-    // res.render('index', { title: 'BlockChain of Noverish!!!', body: body });
 });
 
 module.exports = router;
